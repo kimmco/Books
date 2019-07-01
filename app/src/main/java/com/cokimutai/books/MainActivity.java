784350f4e9fad88aea522ee7572816854cfaefed
cokimutai <cokimutai@gmail.com>
2019-07-01 10:14:23 +0300

    better user experience

--- app/src/main/java/com/cokimutai/books/MainActivity.java
+++ app/src/main/java/com/cokimutai/books/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
@@ -11,10 +13,17 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ProgressBar pdLoading;
+    private TextView errorMessage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        errorMessage = findViewById(R.id.tv_error);
+        pdLoading = findViewById(R.id.pb_loading);
 
         try {
             URL bookUrl = ApiUtils.buildUrl("cooking");
@@ -48,7 +57,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             TextView tvResults = (TextView)findViewById(R.id.tvResponse);
+
             tvResults.setText(result);
+            if (result == null){
+                errorMessage.setVisibility(View.VISIBLE);
+                pdLoading.setVisibility(View.INVISIBLE);
+            }
+               pdLoading.setVisibility(View.INVISIBLE);
+
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            pdLoading.setVisibility(View.VISIBLE);
 
         }
     }
